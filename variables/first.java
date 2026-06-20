dd94f56339df9b20926861f5361ff4add86415a3
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-20 13:26:51 +0530

    e:\Java-DSA\Java-DSA\variables\first.java

---
+++ variables/first.java
@@ -0,0 +1,10 @@
+public class first {
+   public first() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Hello world");
+      System.out.println(7);
+      System.out.println("3+4");
+   }
+}
