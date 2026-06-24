df6f569c634ea8b7d06c3cc7f723dfe881bd7316
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-24 22:49:08 +0530

    e:\Java-DSA\Java-DSA\variables\namingvariable.java

---
+++ variables/namingvariable.java
@@ -0,0 +1,84 @@
+// case sensitivity
+// public class namingvariable {
+    
+//     public static void main(String[] args) {
+        
+//         int age = 40;
+//         int AGE = 60;
+
+//         System.out.println(age);
+
+//        System.out.println(AGE);
+//     }
+// }
+
+//starting character
+// public class namingvariable {
+    
+//     public static void main(String[] args) {
+        
+//         int marks = 40;
+//         int Marks = 60;
+
+//         int _marks = 80;
+//         int $marks = 90;
+
+//         System.out.println(marks);
+
+//        System.out.println(Marks);
+
+//        System.out.println(_marks);
+//        System.out.println($marks);
+//     }
+// }
+
+
+// subsequent character
+
+// public class namingvariable {
+    
+//     public static void main(String[] args) {
+        
+//         int height5 = 40;
+//         int height_love = 60;
+
+//         int marks_ = 80;
+//         int marks$ = 90;
+
+//         int heightlover$20 = 90;
+
+//         System.out.println(height5);
+
+//        System.out.println(height_love);
+
+//        System.out.println(marks_);
+//        System.out.println(marks$);
+//        System.out.println(heightlover$20);
+//     }
+// }
+
+
+
+//reserver keyword --> specific meaning
+// public class namingvariable {
+    
+//     public static void main(String[] args) {
+       
+// int class = 5; // show error 
+       
+//        System.out.println(class);
+//     }
+// }
+
+// main is not reserved keyword
+public class namingvariable {
+    
+    public static void main(String[] args) {
+       
+int main = 5; // show error 
+       
+       System.out.println(main);
+    }
+}
+
+
