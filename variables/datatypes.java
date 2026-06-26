2ddd329c2c15078402b72288ddbefddf3aee8835
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-26 22:39:08 +0530

    e:\Java-DSA\Java-DSA\variables\datatypes.java

---
+++ variables/datatypes.java
@@ -0,0 +1,71 @@
+// public class datatypes {
+    
+//     public static void main(String[] args) {
+        
+//         // primitive data type ---> numeric -->Integer --> byte, short,int long
+
+
+//         byte nums1 = 127;
+
+//         int num = 1000000;
+
+//         short nums = 10000;
+
+//          long nums2 = 1000000000;
+
+//          // floating --> float , double
+
+//          float nums4 = 7.9898980f;
+
+//          double  nums5 = 8.987654321987654321;
+
+//          // non numeric type --> boolean , char
+
+//          boolean eligiblevote = true;
+
+
+//          char firstcharacter = 'a';
+//          System.out.println("my character is: "+(char) (firstcharacter+2));
+
+//          System.out.println(eligiblevote);
+
+//          System.out.println(nums5);
+
+//          System.out.println(nums4);
+
+//          System.out.println(nums2);
+
+//         System.out.println(nums);
+
+//         System.out.println(num);
+
+//         System.out.println(nums1);
+//     }
+// }
+
+
+// datatype implicit and explicit conversion-->typecasting
+
+// ---->IMPLICIT 
+// public class datatypes{
+//     public static void main(String[] args) {
+        
+//         byte nums = 127;
+//         long newNum = num1
+//         System.out.println("newNum : "+ newNum);
+
+//         System.out.println(num1);
+
+//     }
+// }
+
+//EXPLICT
+public class datatypes{
+    public static void main(String[] args) {
+        
+       long value1 = 1234567890;
+       int value2 = (int)value1;
+       System.out.println(value2);
+
+    }
+}
